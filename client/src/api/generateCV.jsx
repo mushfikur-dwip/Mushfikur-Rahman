b45f358034acb50ebe113bf15ee7dcc24c834acd
mushfikur-dwip <musfikurrahmandip@gmail.com>
2025-06-26 19:07:20 +0600

    refactor: update prompt for CV generation and change AI model version

--- client/src/api/generateCV.jsx
+++ client/src/api/generateCV.jsx
@@ -3,12 +3,12 @@ const ai = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
 
 const generateCV = async (userInput) => {
   const prompt = `
-You are a resume expert AI. user your best Try to extract structured information from the following input and return it as valid JSONb format.
+You are a resume expert AI. Try to extract structured information from the following input and return it as valid JSONb format.
 You will receive a text input that contains various details about a person, including their name, title, location, contact links, about section, experience, and education. Your task is to parse this information and return it in the specified JSONb format.
 Always replay in English and even if  the input is in another language, you should still return the output in English.
 Here is the input format you should follow:
 Return ONLY valid JSONb  based on this input — no explanation or text outside JSON.
- 
+
 Format:
 {
   "name": "",
@@ -44,7 +44,7 @@ Now generate JSON for this person:
 `;
 
   try {
-    const model = ai.getGenerativeModel({ model: "gemini-2.0-flash" });
+    const model = ai.getGenerativeModel({ model: "gemini-1.5-flash" });
 
     const result = await model.generateContent(prompt);
     const textOutput = await result.response.text();
